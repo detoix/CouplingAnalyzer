babfacb32c58621cde5dbd1b21ce8988bf79a67d
detoix <dk14yo@gmail.com>
2022-03-31 19:31:40 +0200

    fix test

--- CouplingAnalyzer.Tests/Tests.cs
+++ CouplingAnalyzer.Tests/Tests.cs
@@ -12,12 +12,18 @@ namespace CouplingAnalyzer.Tests
         {
             var directory = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent}.Resources";
             var projectDirectoryName = Path.GetFileName(directory);
+            var expectedPath = Path.Combine(Directory.GetCurrentDirectory(), $"{projectDirectoryName}.tsv");
+            var actualPath = Path.Combine(directory, $"{projectDirectoryName}.tsv");
+
+            if (File.Exists(actualPath))
+            {
+                File.Delete(actualPath);
+            }
 
             await Program.Main(new[] { Path.Combine(directory, $"{projectDirectoryName}.sln"), nameof(GeneratedReportIsEqualToExpected) });
 
-            var expected = File.ReadAllLines(
-                Path.Combine(Directory.GetCurrentDirectory(), $"{projectDirectoryName}.tsv"));
-            var actual = File.ReadAllLines(Path.Combine(directory, $"{projectDirectoryName}.tsv"));
+            var expected = File.ReadAllLines(expectedPath);
+            var actual = File.ReadAllLines(actualPath);
             
             CollectionAssert.AreEqual(expected, actual);
         }
